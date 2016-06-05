a715c2642149313b4fea0d46df7a19273d1168b1
Nacho <ignacio.palacios.santos@gmail.com>
2016-06-06 01:04:24 +0200

    struggling with nesting

--- lib/dry_i18n.rb
+++ lib/dry_i18n.rb
@@ -2,20 +2,38 @@ require "dry_i18n/version"
 require "i18n"
 
 module DryI18n
-    I18n::Backend::Base.module_eval do
-      alias_method :original_translate, :translate
-      def translate(locale, key, options = {})
-        result = original_translate(locale, key, options = {})
-        nested_words = result.scan(/\@{.*?\}/)
-
-        nested_words.each do |nested_word|
-          if !nested_word.nil?
-            word = nested_word[/@{(.*?)}/m, 1]
-            translated_word = original_translate(locale, word, options = {})
-            result = result.sub(nested_word, translated_word)
-          end
-        end
-        result
-      end
+  I18n::Backend::Base.module_eval do
+    alias_method :original_translate, :translate
+    def translate(locale, key, options = {})
+      result = original_translate(locale, key, options)
+      nested_words = result.scan(/\@\[.*?\]/)
+      translate_nested(nested_words, result, locale)
     end
+
+    def translate_nested(nested_words, result, locale)
+      nested_words.each do |nested_word|
+        key = nested_word[/@\[(.*?)\]/m, 1]
+        binding.pry
+        begin
+          key_with_options = eval(key)
+          main_key = key_with_options.keys.first
+          options = key_with_options[main_key]
+          options_sym = {}
+          options.each do |k,v|
+            options_sym[k.to_sym] = v
+          end
+        rescue
+          main_key = key
+        ensure
+          translated_key = original_translate(locale, main_key, options_sym ||= {})
+          sub_keys = translated_key.scan(/\@\[.*?\]/)
+          if sub_keys.any?
+            translated_key = translate_nested(sub_keys, translated_key, locale)
+          end
+          result = result.sub(nested_word, translated_key)
+        end
+      end
+      result
+    end
+  end
 end
