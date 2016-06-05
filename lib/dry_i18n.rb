a39971d1c041ed6ac25117c16b52a6c5b2a57627
Nacho <ignacio.palacios.santos@gmail.com>
2016-06-05 18:14:23 +0200

    working basic functionality

--- lib/dry_i18n.rb
+++ lib/dry_i18n.rb
@@ -1,10 +1,20 @@
 require "dry_i18n/version"
-require 'i18n'
+require "i18n"
 module DryI18n
-  class Test
-    def translate word
-      puts I18n.t(word)
+    I18n::Backend::Base.module_eval do
+      alias_method :original_translate, :translate
+      def translate(locale, key, options = {})
+        result = original_translate(locale, key, options = {})
+        nested_words = result.scan(/\@{.*?\}/)
+
+        nested_words.each do |nested_word|
+          if !nested_word.nil?
+            word = nested_word[/@{(.*?)}/m, 1]
+            translated_word = original_translate(locale, word, options = {})
+            result = result.sub(nested_word, translated_word)
+          end
+        end
+        result
+      end
     end
-  end
-  # Your code goes here...
 end
